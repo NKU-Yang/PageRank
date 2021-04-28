abcd5d45e6e9ec3f18a4c0b0528c55bfdcf444ea
NKU-Yang <2759838841@qq.com>
2021-04-28 23:26:03 +0800

    right result

--- PageRank/PageRank.cpp
+++ PageRank/PageRank.cpp
@@ -11,77 +11,80 @@
 #include<math.h>
 using namespace std;
 
-const double STOPLOSS =0.01;
-typedef unsigned short Node_ID;
+const double STOPLOSS = 1e-10;
+typedef unsigned int Node_ID;
 typedef vector<Node_ID> Links;
 typedef map<Node_ID, Links> Page;
- vector<int>dgree;
- Page P;
- typedef vector<double>Rank;
- Rank New;
- Rank Old;
- int max(unsigned short a, unsigned short b)
- {
-	 return a > b ? a : b;
- }
- //得到初始链接和节点度数
+vector<int>dgree;
+vector<int>out_dgree;
+Page P;
+Page P_out;
+typedef vector<double>Rank;
+Rank New;
+Rank Old;
+Node_ID maxID = 0;
+int max(unsigned short a, unsigned short b)
+{
+	return a > b ? a : b;
+}
+//得到初始链接和节点度数
 int Init(const char* filename)
 {
-	fstream input(filename,ios::in);
-	Node_ID from = 0; 
-	Node_ID to=0;
-	//Node_ID maxID = 0;
+	fstream input(filename);
+	Node_ID from = 0;
+	Node_ID to = 0;
 
-	int maxID=0;
-	while (!input.eof())
+	while (!input.eof() && input >> from >> to)
 	{
-		input >> from >> to;
 		P[to].push_back(from);
+		P_out[from].push_back(to);
 		maxID = max(maxID, max(from, to));
 	}
 	input.close();
-	dgree.resize(maxID+1);
-	for (int i = 0; i < maxID+1; i++)
+	dgree.resize(maxID + 1);
+	out_dgree.resize(maxID + 1);
+	for (int i = 0; i < maxID + 1; i++)
 	{
-		dgree[i]=P[i].size();
+		dgree[i] = P[i].size();
+		out_dgree[i] = P_out[i].size();
 	}
-	return maxID+1;
+	int count = maxID;
+	for (int i = 0; i < maxID + 1; i++)
+	{
+		if (dgree[i] == 0 && out_dgree[i] == 0)
+			count--;
+	}
+	return count + 1;
 }
-////得到初始化矩阵
-//void InitMatrix(int n,double teleport,double** M)
-//{
-//	for (auto i : P)
-//	{
-//		for (auto j : i.second)
-//		{
-//			M[j][i.first] = teleport * (Old[i.first] / dgree[i.first]);
-//		}
-//	}
-//	for (int i = 0; i < n; i++)
-//	{
-//		for (int j = 0; j < n; j++)
-//		{
-//			M[i][j] +=(1.0-teleport)*(1.0/n);
-//		}
-//	}
-//	return ;
-//}
+
 //计算PageRank
-void ComputeRank(int n,double teleport,double** M)
+void ComputeRank(int n, double teleport)
 {
-	New.resize(n, 0);
-	Old.resize(n,1);
+	New.resize(maxID + 1, 0);
+	Old.resize(maxID + 1, 1.0 / n);
 	double loss = 0.0;
 	do {
 		loss = 0.0;
 		for (auto i : P)
 		{
 			New[i.first] = 0.0;
-			for (auto j : i.second)
+			for (int j = 0; j < i.second.size(); j++)
 			{
-				New[i.first] += teleport * (Old[j] / dgree[j]) ;
+				if (out_dgree[i.second[j]] != 0)
+					New[i.first] += teleport * (Old[i.second[j]] / out_dgree[i.second[j]]);
+				else
+					New[i.first] += 0;
 			}
-			New[i.first] += (1.0 / n);
+		}
+		double S = 0.0;
+		for (auto i : New)
+		{
+			S += i;
+		}
+		//cout << "curr S: " << S << endl;
+		for (int i = 0; i < New.size(); i++)
+		{
+			New[i] += (1.0 - S) / n;
 		}
 		for (int i = 0; i < New.size(); i++)
 		{
@@ -99,9 +102,15 @@ void ComputeRank(int n,double teleport,double** M)
 		{
 			Old[i] = New[i];
 		}
-		//cout << "curr loss:" << loss << endl;
-	} while (loss>STOPLOSS);
+		cout << "curr loss:" << loss << endl;
+	} while (loss > STOPLOSS);
 	cout << "finale loss:" << loss << endl;
+	ofstream res("result.txt");
+	for (auto i : P)
+	{
+		res << i.first << "\t" << New[i.first] << endl;
+	}
+	res.close();
 	return;
 }
 int main()
@@ -109,27 +118,15 @@ int main()
 	const char* filename = "WikiData.txt";
 	double teleport = 0.85;
 	int count = Init(filename);
-	double** M = new double* [count];
-
-	for (int i = 0; i < count; i++)
-	{
-		M[i] = new double[count];
-	}
-	for (int i = 0; i < count; i++)
-	{
-		for (int j = 0; j < count; j++)
-		{
-			M[i][j] = 0;
-		}
-	}
-	ComputeRank(count, teleport, M);
-	ofstream out("result.txt");
+	cout << endl;
+	ComputeRank(count, teleport);
+	ofstream out("dgree.txt");
 	for (auto i : P)
 	{
-		out << i.first <<"\t"<< New[i.first] << endl;
+		out << i.first << "\t" << "out_dgree: " << out_dgree[i.first] << "\tin_dgree: " << dgree[i.first] << endl;
 	}
 	out.close();
-	
+
 	return 0;
 }
 
